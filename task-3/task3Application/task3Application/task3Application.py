2c409de2f72d21d694fdc83047f28e278d1c2ed6
Daniil luchitskiy <luchikholiday2@gmail.com>
2021-07-12 19:14:52 +0300

    task-3 added methods that returns finaly data

--- task-3/task3Application/task3Application/task3Application.py
+++ task-3/task3Application/task3Application/task3Application.py
@@ -1,18 +1,42 @@
 import http.client
 import json
 
-conn = http.client.HTTPSConnection("vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com")
+def getCountriesList():
+    connect = http.client.HTTPSConnection("vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com")
 
-headers = {
-    'x-rapidapi-key': "84ddb881f1mshe52cf4b1b8fbde2p1665f2jsnc71b246a3a81",
-    'x-rapidapi-host': "vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com"
-    }
+    headers = {
+        'x-rapidapi-key': "84ddb881f1mshe52cf4b1b8fbde2p1665f2jsnc71b246a3a81",
+        'x-rapidapi-host': "vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com"
+        }
 
-conn.request("GET", "/api/covid-ovid-data/sixmonth/UKR", headers=headers)
+    connect.request("GET", "/api/npm-covid-data/", headers=headers)
 
-res = conn.getresponse()
-data = res.read()
+    res = connect.getresponse()
+    data = res.read()
 
-parsedData = json.loads(data.decode("utf-8"))
+    return list(json.loads(data.decode("utf-8")))
 
-print(parsedData)
+def getCountryStat(CountryIdentifyer):
+
+    conn = http.client.HTTPSConnection("vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com")
+
+    headers = {
+        'x-rapidapi-key': "84ddb881f1mshe52cf4b1b8fbde2p1665f2jsnc71b246a3a81",
+        'x-rapidapi-host': "vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com"
+        }
+
+    conn.request("GET", "/api/covid-ovid-data/sixmonth/" + CountryIdentifyer, headers=headers)
+
+    res = conn.getresponse()
+    data = res.read()
+
+    if data.decode("utf-8") == '[]':
+        CountryList = getCountriesList()
+        for i in range(len(CountryList)):
+            if str(dict(CountryList[i]).get("TwoLetterSymbol")).lower() == CountryIdentifyer.lower() or str(dict(CountryList[i]).get("Country")).lower() == CountryIdentifyer.lower():
+                return list(getCountryStat(dict(CountryList[i]).get("ThreeLetterSymbol")))
+        return ""
+    else:
+        return list(json.loads(data.decode("utf-8")))
+
+print(getCountryStat("ukraine")[0])
